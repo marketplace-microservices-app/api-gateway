4fe67d305cb3a2a456664e758485305d9b254998
Dewmith Akalanka <akalankadewmith@gmail.com>
2025-04-19 09:54:53 +0530

    add login api to gateway

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
 import { Inject } from '@nestjs/common';
 import { RegisterUserPayload } from './types/RegisterUserPayload.interface';
+import { LoginUserPayload } from './types/LoginUserPayload.interface';
 
 @Controller('api/auth')
 export class AuthController {
@@ -11,11 +12,17 @@ export class AuthController {
 
   async onModuleInit() {
     this.authClient.subscribeToResponseOf('auth.register');
+    this.authClient.subscribeToResponseOf('auth.login');
     await this.authClient.connect();
   }
 
   @Post('register')
   async register(@Body() body: RegisterUserPayload) {
     return this.authClient.send('auth.register', body).toPromise();
+  }
+
+  @Post('login')
+  async login(@Body() body: LoginUserPayload) {
+    return this.authClient.send('auth.login', body).toPromise();
   }
 }
