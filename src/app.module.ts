b86bfc2619498e88835a0ee389a21bba6eee9b9e
Dewmith Akalanka <akalankadewmith@gmail.com>
2025-04-19 06:46:27 +0530

    initial commit with 1 ms connectivity check

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AuthController } from './auth/auth.controller';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'api-gateway',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'api-gateway-auth-consumer',
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [AppController, AuthController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { Inject } from '@nestjs/common';
+
+@Controller('api/auth')
+export class AuthController {
+  constructor(
+    @Inject('AUTH_SERVICE') private readonly authClient: ClientKafka,
+  ) {}
+
+  async onModuleInit() {
+    this.authClient.subscribeToResponseOf('auth.register');
+    await this.authClient.connect();
+  }
+
+  @Post('register')
+  async register(@Body() body: { email: string; password: string }) {
+    return this.authClient.send('auth.register', body).toPromise();
+  }
+}
