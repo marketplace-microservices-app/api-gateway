3da2d8468518a6fff218bda8223fb7d2979acd6c
Dewmith Akalanka <akalankadewmith@gmail.com>
2025-04-20 23:48:17 +0530

    add get all orders by user id

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -6,11 +6,17 @@ import { CreateOrderPayload } from './types/CreateOrderPayload';
 export class OrderController {
   constructor(
     @Inject('ORDER_SERVICE') private readonly orderClient: ClientKafka,
+    @Inject('USERS_SERVICE') private readonly userClient: ClientKafka,
   ) {}
 
   async onModuleInit() {
     this.orderClient.subscribeToResponseOf('order.create');
     this.orderClient.subscribeToResponseOf('order.cancel');
+    this.orderClient.subscribeToResponseOf('order.get-all-orders-by-userId');
+    this.orderClient.subscribeToResponseOf('order.get-orders-by-buyerId');
+    this.userClient.subscribeToResponseOf(
+      'users.get-buyer-details-from-userId',
+    );
     await this.orderClient.connect();
   }
 
@@ -22,5 +28,25 @@ export class OrderController {
   @Post('cancel')
   async cancel(@Body() orderId: string) {
     return this.orderClient.send('order.cancel', orderId).toPromise();
+  }
+
+  @Post('get-all-orders-by-userId')
+  async getAllOrdersByUserId(@Body() userId: string) {
+    // Get buyer_id from the user_id
+
+    const buyerDetails = await this.userClient
+      .send('users.get-buyer-details-from-userId', userId)
+      .toPromise();
+
+    const buyerId = buyerDetails?.data.id;
+    if (!buyerId) {
+      throw new Error('Buyer ID not found for the given user ID');
+    }
+
+    const orders = await this.orderClient
+      .send('order.get-orders-by-buyerId', buyerId)
+      .toPromise();
+
+    return orders;
   }
 }
